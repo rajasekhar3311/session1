398c9129620e39539f840a84f9fbaa036b653e99
rajasekhar3311 <rajasekhar3311@gmail.com>
2017-04-24 11:20:32 +0530

    dbcnbjdb

--- src/java/net/sourceforge/cruisecontrol/sampleproject/connectfour/Cell.java
+++ src/java/net/sourceforge/cruisecontrol/sampleproject/connectfour/Cell.java
@@ -9,7 +9,7 @@
  * modification, are permitted provided that the following conditions
  * are met:
  *
- *     + Redistributions of source code must retain the above copyright
+ *     + Redistribution of source code must retain the above copyright
  *       notice, this list of conditions and the following disclaimer.
  *
  *     + Redistributions in binary form must reproduce the above
@@ -19,7 +19,7 @@
  *
  *     + Neither the name of ThoughtWorks, Inc., CruiseControl, nor the
  *       names of its contributors may be used to endorse or promote
- *       products derived from this software without specific prior
+ *       products derived from this software without specific priority
  *       written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
