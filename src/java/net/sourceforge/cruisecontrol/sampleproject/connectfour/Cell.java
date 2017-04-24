d758af92383a6497a20ddac3488eae7533ab24ae
gunampalli3311 <rajasekhar3311@gmail.com>
2017-04-24 07:00:16 +0530

    hfj

--- src/java/net/sourceforge/cruisecontrol/sampleproject/connectfour/Cell.java
+++ src/java/net/sourceforge/cruisecontrol/sampleproject/connectfour/Cell.java
@@ -15,7 +15,7 @@
  *     + Redistributions in binary form must reproduce the above
  *       copyright notice, this list of conditions and the following
  *       disclaimer in the documentation and/or other materials provided
- *       with the distribution.
+ *       with the distributions.
  *
  *     + Neither the name of ThoughtWorks, Inc., CruiseControl, nor the
  *       names of its contributors may be used to endorse or promote
